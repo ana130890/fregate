f683446e5b65ac2682b361c92ca56c919b033a03
pierrik <plassalas@users.noreply.github.com>
2017-05-11 16:49:04 +0200

    Correction

--- src/fr/gtm/fregate/Hello.java
+++ src/fr/gtm/fregate/Hello.java
@@ -4,14 +4,11 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, GIT");
-<<<<<<< HEAD
-		System.out.println("une bi�re, Ana??");
+		System.out.println("une bière, Ana??");
 		System.out.println("vodkaaaaaa?");
 
-=======
 		System.out.println("une bi�re, Ana??");
 		System.out.println("Modification abusée");
->>>>>>> branch 'master' of https://github.com/ana130890/fregate
 	}
 
 }
