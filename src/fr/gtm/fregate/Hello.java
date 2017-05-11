aecc557c3e8e8df9bb2ba3f7359f468ed644401f
pierrik <plassalas@users.noreply.github.com>
2017-05-11 16:38:18 +0200

    commit

--- src/fr/gtm/fregate/Hello.java
+++ src/fr/gtm/fregate/Hello.java
@@ -4,8 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, GIT");
-		System.out.println("une bière, Ana??");
-
+		System.out.println("une biï¿½re, Ana??");
+		System.out.println("Modification abusÃ©e");
 	}
 
 }
